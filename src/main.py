57e5f55beb09f21709eb7d714a68d8e15d15635c
Vitah <vumirek@gmail.com>
2023-05-11 02:47:06 +0200

    configure nginx.conf

--- src/main.py
+++ src/main.py
@@ -43,6 +43,7 @@ if __name__ == "__main__":
 
     uvicorn.run(
         app,
+        host="0.0.0.0",
         ssl_certfile=config("SSL_CERTFILE"),
         ssl_keyfile=config("SSL_KEYFILE"),
     )
